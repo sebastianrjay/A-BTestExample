c3aca9d8db3a27838ef33b9a0bb77438e9e5aad6
Sebastian Jay <sebastianrjay@gmail.com>
2015-11-06 12:06:40 -0800

    Continue debugging Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
 
   def current_user
     gmail_token, expiration = cookies[:gmail_token], cookies[:expires_at].to_i

1be3c6012a54a734a46ed3e71b11af0e825180a9
Sebastian Jay <sebastianrjay@gmail.com>
2015-11-06 15:46:10 -0800

    Debug URL encryption

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,7 +3,7 @@ class InvitesController < ApplicationController
   def show
     # Change template firstName to @referring_user.first_name, gmailAddress
     # to @referring_user.gmail_address
-    @referring_user = User.find_by_gmail_address(params[:id])
+    @referring_user = User.find_by_gmail_address(URLcrypt.decrypt(params[:id]))
     cookies[:invited_user_cookie] ||= SecureRandom.urlsafe_base64
 
     unless (cookies[:referring_users] || []).include?(@referring_user.gmail_address)

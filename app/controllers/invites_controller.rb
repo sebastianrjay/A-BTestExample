e47539a7874f112149eed43db3f7eb7991ce08a8
Sebastian Jay <sebastianrjay@gmail.com>
2015-11-07 11:07:00 -0800

    Improve comment in InvitesController

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,15 +1,17 @@
 class InvitesController < ApplicationController
 
   def show
-    # Change template firstName to @referring_user.first_name, gmailAddress
-    # to @referring_user.gmail_address
     @referring_user = User
       .find_by_gmail_address(Base64.urlsafe_decode64(params[:id])) rescue nil
 
     cookies[:invited_user_cookie] ||= SecureRandom.urlsafe_base64
 
-    # Save a new invite if the user has not logged in before, and has not opened
-    # this invite page before
+    # Save a new invite to the DB if the user has not logged in before, and
+    # either has not opened this invite page before, or has been referred by an
+    # additional user. Notice that we save a unique invite for each user who
+    # sends the invite link to the same person. If I'm invited by Bob and Sally,
+    # I've received two invites. I could change the app so that it only saves
+    # one invite per invited user; I somewhat arbitrarily chose this strategy.
     if @referring_user && !cookies[:gmail_token] &&
         !(cookies[:referring_users] || []).include?(@referring_user.gmail_address)
       cookies[:referring_users] ||= []
